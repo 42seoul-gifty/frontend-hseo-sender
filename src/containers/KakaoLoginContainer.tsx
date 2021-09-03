62efa2d8b9f28cdfa33a22685d92ec0ed58caa28
HEEYOUNG SEO <hseo@HEEYOUNGs-iMac.local>
2021-09-04 00:01:42 +0900

    feat: getToken 함수 추가

--- src/containers/KakaoLoginContainer.tsx
+++ src/containers/KakaoLoginContainer.tsx
@@ -1,13 +1,37 @@
 import React from 'react'
 import { css } from '@emotion/react'
+import { useEffect } from 'react'
 import Modal from 'components/Modal'
+import { BASE_URL, CLIENT_ID, REDIRECT_URI } from 'config'
+import axios from 'axios'
 
 const KakaoLoginContainer: React.FC = () => {
-  const code = new URL(window.location.href).searchParams.get('code')
+  useEffect(() => {
+    const code = new URL(window.location.href).searchParams.get('code')
+    if (!code) {
+      return
+    }
+    const kakaoUrl = 'https://kauth.kakao.com/oauth/token'
+    const data = {
+      grant_type: 'authorization_code',
+      cilent_id: CLIENT_ID,
+      redirect_uri: REDIRECT_URI,
+      code: code,
+    }
+
+    const getToken = async (code: string) => {
+      const res = await axios.post(kakaoUrl, data)
+      console.log(res)
+      return res
+    }
+
+    getToken(code)
+    window.location.assign('/main')
+  }, [])
 
   return (
     <div css={Container}>
-      <h1>카카오 redirected {code}</h1>
+      <h1>카카오 redirected</h1>
       <Modal />
     </div>
   )
