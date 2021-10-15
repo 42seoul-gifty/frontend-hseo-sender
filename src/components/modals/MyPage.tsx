ca252eb54a7a61d2a24805d0bafd321472581d61
hyseoseo <heeyoungseo83@gmail.com>
2021-10-16 00:42:12 +0900

    feat: logout 구현

--- src/components/modals/MyPage.tsx
+++ src/components/modals/MyPage.tsx
@@ -10,9 +10,12 @@ import {
   SHOW_POLICY_MODAL,
   SHOW_PRIVACY_MODAL,
 } from 'store/actions/modal'
+import api from 'api'
+import { useHistory } from 'react-router'
 
 const MyPage: React.FC = () => {
   const dispatch = useDispatch()
+  const history = useHistory()
 
   const handleModalButton = (typename: ModalType) => {
     dispatch(hideModal())
@@ -23,6 +26,32 @@ const MyPage: React.FC = () => {
     window.open('https://gifty4u.co.kr', '_blank')
   }
 
+  const handleLogout = async () => {
+    const refreshToken = localStorage.getItem('refresh_token')
+    const accessToken = localStorage.getItem('access_token')
+    try {
+      const res = await api({
+        method: 'post',
+        url: `/logout`,
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+        data: {
+          refresh_token: refreshToken,
+        },
+      })
+      console.log(res.data)
+      dispatch(hideModal())
+      localStorage.setItem('user_id', '')
+      localStorage.setItem('refresh_token', '')
+      localStorage.setItem('access_token', '')
+      localStorage.setItem('nickname', '')
+      alert('로그아웃 되었습니다')
+      history.push('/')
+    } catch (e) {
+      console.log(e)
+    }
+  }
   return (
     <div css={Container}>
       <h1>{localStorage.getItem('nickname')}님 안녕하세요!</h1>
@@ -33,7 +62,7 @@ const MyPage: React.FC = () => {
         정보보안규약
       </button>
       <button onClick={handlePresentationButton}>기프티소개</button>
-      <button>로그아웃</button>
+      <button onClick={handleLogout}>로그아웃</button>
     </div>
   )
 }
